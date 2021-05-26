e590520e9bd4e2bb731bd8772f73399f32baa672
Moon Beob Sik <tjswl756@naver.com>
2021-05-26 21:22:28 +0900

    feat:delete unused code

--- src/syntax_analyzer/SLRparsing.java
+++ src/syntax_analyzer/SLRparsing.java
@@ -23,15 +23,11 @@ public class SLRparsing {
     JSONArray InputArray = new JSONArray();
     JSONObject InputObject = new JSONObject();
 
-    Stack<String> InputStack=new Stack<>();//삭제할 것
-
     //Stack for storing states
     Stack<Integer> stack = new Stack<>();
 
     String nextInputSymbol = "";
     int splitter;
-
-    String ShowInputString = "";//삭제할 것
 
     //Store lexical analyzer input file name in variable used when an error occurs
     String lexerInputFileNameForError;
@@ -47,18 +43,12 @@ public class SLRparsing {
         InitInputArray();
         InitSLRTable();
         InitCFGArray();
-        InitShowInputString();//삭제할 것
-        System.out.println(ShowInputString);//삭제할 것
 
         //Repeat statement to recognize input
         while (splitter < InputArray.size()) {
             //Get InputObject based on splitter and store terminal of InputObject to nextInputSymbol
             InputObject = (JSONObject) InputArray.get(splitter);
             nextInputSymbol = InputObject.get("Terminals").toString();
-
-            ShowStack();//삭제할 것
-            System.out.println("Input Symbol:" + nextInputSymbol);//삭제할 것
-            InputStack.push(nextInputSymbol);//삭제할 것
 
             //Get decision based on the current state and next input symbol
             decision = MakeDecision(stack.peek(), nextInputSymbol);
@@ -68,8 +58,6 @@ public class SLRparsing {
                 System.out.println("Accept");
                 break;
             }
-
-            System.out.println("decision:" + decision);//삭제할 것
 
             //Distinguish whether decision is reduce or shift
             switch (decision.charAt(0)) {
@@ -81,30 +69,17 @@ public class SLRparsing {
                     int CFGSize = Integer.parseInt(CFGObject.get("size").toString());
                     for (int i = 0; i < CFGSize; i++) {
                         stack.pop();
-                        InputStack.pop();//삭제할 것
                     }
-
-                    InputStack.pop();//삭제할 것
-                    ShowStack();//삭제할 것
-                    System.out.println("Replace[left]:" + CFGObject.get("left").toString() + ", Replace[right]:" + CFGObject.get("right").toString());//삭제할 것
 
                     //Push GOTO number into the stack based on the current state and left-hand side of the CFG
                     String GOTO = MakeDecision(stack.peek(), CFGObject.get("left").toString());
                     stack.push(Integer.parseInt(GOTO));
-
-                    System.out.println();//삭제할 것
-                    InputStack.push(CFGObject.get("left").toString());//삭제할 것
                 }
                 case 's' -> {
-                    System.out.println();//삭제할 것
                     //Push state into the stack and move the splitter to the right
                     stack.push(Integer.parseInt(decision.substring(1)));
                     splitter++;
                 }
-                /*default -> {
-                    System.out.println();
-                    stack.push(Integer.parseInt(decision.substring(1)));
-                }*/
             }
         }
     }
@@ -188,27 +163,6 @@ public class SLRparsing {
         System.out.println("SyntaxError:    Unexpected token -> "+unexpectedToken);
         System.out.println("Error at:"+getOneLine+"     ("+lexerInputFileNameForError+": Line "+rowOfError+")");
     }
-
-    //삭제할것--------------------------------------------------
-    public void InitShowInputString() {
-        for (Object o : InputArray) {
-            InputObject = (JSONObject) o;
-            ShowInputString = ShowInputString.concat(InputObject.get("Terminals").toString());
-            ShowInputString = ShowInputString.concat(" ");
-        }
-    }
-
-    public void ShowStack() {
-        int j=0;
-        for (int i = 0; i < stack.size(); i++) {
-            System.out.print(stack.elementAt(i) + " ");
-            if(j<InputStack.size())
-                System.out.print(InputStack.elementAt(j++)+" ");
-        }
-        System.out.println();
-    }
-    //----------------------------------------------------------
-
 
     public String MakeDecision(int row, String inputSymbol) {
         //Get SLRTableRow from the SLRTable based on the state
